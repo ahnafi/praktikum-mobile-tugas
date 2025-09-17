bc0e648ac6bd6579f1c119dd0c92ecc57f8ecb43
ahnafi <devsulthon@gmail.com>
2025-09-17 08:42:12 +0700

    Add : Tugas 2 Pertemuan 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.praktikum.prakpemob"
     compileSdk = 36
 
+    viewBinding {
+        enable = true
+    }
+
     defaultConfig {
         applicationId = "com.praktikum.prakpemob"
         minSdk = 24
